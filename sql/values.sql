93a6449c229305c6a9c5c1e795c23231eca54a2a
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-11-27 13:07:48 +0100

    moved db to global server

--- sql/values.sql
+++ sql/values.sql
@@ -1,18 +1,20 @@
 delete from Users;
 delete from Roles;
 
-insert into Roles(rolename)
-values
-('Admin'),
-('Teacher'),
-('Student')
-;
+INSERT INTO ROLES (ROLENAME)
+VALUES ('admin');
+INSERT INTO ROLES (ROLENAME)
+VALUES ('teacher');
+INSERT INTO ROLES (ROLENAME)
+VALUES ('student');
+
 
 insert into Users(id, password, role)
 values
-('admin@test.com', 'test', 'Admin'),
-('teacher@test.com', 'test', 'Teacher'),
-('student@test.com', 'test', 'Student')
-;
-
-
+('admin@test.com', 'test', 'admin');
+insert into Users(id, password, role)
+values
+('teacher@test.com', 'test', 'teacher');
+insert into Users(id, password, role)
+values
+('student@test.com', 'test', 'student');
